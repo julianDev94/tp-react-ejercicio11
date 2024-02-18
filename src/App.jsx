267ce26fcc4284c1b02e04b17af414d3eb9e676e
JulianAballay <juliancabj1210@gmail.com>
2024-02-18 13:41:31 -0300

    agrego maquetado titulo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import Titulo from './components/Titulo'
 
 function App() {
   
 
   return (
     <>
-      
+      <section className='text-center border border-dark p-3 mb-3 bg-secondary bg-gradient text-white'>
+        <Titulo />
+      </section>
     </>
   )
 }
---
+++ src/components/Titulo.jsx
@@ -0,0 +1,8 @@
+
+const Titulo = () => {
+    return (
+        <h1 className="display-1">Noticias</h1>
+    );
+};
+
+export default Titulo;
