3f59dfe66baf7d898f200e43a45c62243397016b
JulianAballay <juliancabj1210@gmail.com>
2024-02-22 12:22:48 -0300

    comento el codigo de filtradocategoria

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -4,23 +4,23 @@ import ListaNoticias from "./ListaNoticias";
 
 const Formulario = () => {
   const [noticias, setNoticias] = useState([]);
-  const [categoria, setCategoria] = useState([]);
+  // const [categoria, setCategoria] = useState([]);
 
   useEffect(() => {
     consultarAPI();
     // filtrarCategoria();
   },[]);
 
-  const filtrarCategoria = () =>{
-    const categorias = [];
-    for(let i = 0; i<noticias.length;i++){
-      categorias.push(noticias[i].category[0]);
-    }
-    const categoriaFiltrada = new Set(categorias);
-    const resultadoCategoriaFiltrada = [...categoriaFiltrada];
-    console.log(resultadoCategoriaFiltrada);
-    setCategoria(resultadoCategoriaFiltrada);
-  }
+  // const filtrarCategoria = () =>{
+  //   const categorias = [];
+  //   for(let i = 0; i<noticias.length;i++){
+  //     categorias.push(noticias[i].category[0]);
+  //   }
+  //   const categoriaFiltrada = new Set(categorias);
+  //   const resultadoCategoriaFiltrada = [...categoriaFiltrada];
+  //   console.log(resultadoCategoriaFiltrada);
+  //   setCategoria(resultadoCategoriaFiltrada);
+  // }
 
   const consultarAPI = async () => {
     const APIkey = "pub_38512e2708dc7ad15dfa9cad85c582a9c4ec1";
@@ -41,9 +41,9 @@ const Formulario = () => {
           </Form.Label>
           <Col sm="9">
             <Form.Select >
-              {
+              {/* {
                 categoria.map((cate)=><option value={cate}>{cate}</option>)
-              }
+              } */}
             </Form.Select>
           </Col>
         </Form.Group>
