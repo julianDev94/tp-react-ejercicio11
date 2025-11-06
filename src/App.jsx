1802bcb55393414285a5136570fb74233e06728c
JulianAballay <juliancabj1210@gmail.com>
2025-11-06 01:02:53 -0300

    agrego logica de componente condicional para mostrar un spinner de loading hasta que se reciban los datos de la API

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Titulo from "./components/Titulo";
-import { Container } from "react-bootstrap";
+import { Container, Spinner } from "react-bootstrap";
 import Formulario from "./components/Formulario";
 import { useEffect } from "react";
-import ListaNoticias from "./components/ListaNoticias.jsx"
+import ListaNoticias from "./components/ListaNoticias.jsx";
 import { useState } from "react";
 
 function App() {
   const [noticias, setNoticias] = useState([]);
   const [categoria, setCategoria] = useState("business");
+  const [spinner, setSpinner] = useState(true);
 
   useEffect(() => {
     consultarAPI();
@@ -21,6 +22,7 @@ function App() {
       const consulta = await fetch(`
     https://newsdata.io/api/1/news?apikey=${APIkey}&language=es&category=${categoria}`);
       const resultado = await consulta.json();
+      setSpinner(false);
       console.log(resultado.results);
       setNoticias(resultado.results);
     } catch (error) {
@@ -28,14 +30,22 @@ function App() {
     }
   };
 
+  const mostrarSpinner = spinner ? (
+    <div className="d-flex justify-content-center">
+      <Spinner animation="grow" variant="success" />
+    </div>
+  ) : (
+    <section className="border border-dark">
+      <ListaNoticias noticias={noticias} />
+    </section>
+  );
+
   return (
     <>
       <Titulo />
       <Container>
-        <Formulario setCategoria={setCategoria}/>
-        <section className="border border-dark">
-          <ListaNoticias noticias={noticias} />
-        </section>
+        <Formulario setCategoria={setCategoria} />
+        {mostrarSpinner}
       </Container>
     </>
   );
