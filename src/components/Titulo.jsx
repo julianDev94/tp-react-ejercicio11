5dc9a07f86e69353259f3b52a119523a83a3bc3c
JulianAballay <juliancabj1210@gmail.com>
2025-11-06 01:09:54 -0300

    le cambio el background del componente titulo

--- src/components/Titulo.jsx
+++ src/components/Titulo.jsx
@@ -1,6 +1,6 @@
 const Titulo = () => {
   return (
-    <section className="text-center border border-dark p-3 mb-3 bg-secondary bg-gradient text-white">
+    <section className="text-center border border-dark p-3 mb-3 bg-success bg-gradient text-white">
       <h1 className="display-1">Noticias</h1>
     </section>
   );
