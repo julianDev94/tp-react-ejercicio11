07d0bac33f339e78daedc2f139d80005b0f9f2d8
JulianAballay <juliancabj1210@gmail.com>
2025-11-06 01:11:31 -0300

    modifico el titulo

--- src/components/Titulo.jsx
+++ src/components/Titulo.jsx
@@ -1,7 +1,7 @@
 const Titulo = () => {
   return (
     <section className="text-center border border-dark p-3 mb-3 bg-success bg-gradient text-white">
-      <h1 className="display-1">Noticias</h1>
+      <h1 className="display-1">Portal de noticias</h1>
     </section>
   );
 };
