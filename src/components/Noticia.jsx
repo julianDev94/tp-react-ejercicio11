a6aaa6f3dd3f19a1124537be772b65817423aed3
JulianAballay <juliancabj1210@gmail.com>
2025-11-06 01:29:36 -0300

    agrego text-center a las columnas de las cargas y ademas justify center para centrar todas las card

--- src/components/Noticia.jsx
+++ src/components/Noticia.jsx
@@ -2,22 +2,22 @@ import { Card, Button, CardFooter, Col, Row } from "react-bootstrap";
 
 const Noticia = ({ noticia }) => {
   return (
-    <Row xs={1} md={2} className="g-4 my-2">
-      {Array.from({ length: 1 }).map((_, idx) => (
-        <Col key={idx}>
-          <Card className="cardNoticia h-100 mx-3">
-            <Card.Img variant="top" src={noticia.image_url} alt="Imagen de noticia no disponible" />
-            <Card.Body>
-              <Card.Title>{noticia.title}</Card.Title>
-              <Card.Text>{noticia.description}</Card.Text>
-            </Card.Body>
-            <CardFooter className="text-center py-3">
-              <Button variant="primary" href={noticia.link}>Ver noticia completa</Button>
-            </CardFooter>
-          </Card>
-        </Col>
-      ))}
-    </Row>
+    <Card className="cardNoticia h-100">
+      <Card.Img
+        variant="top"
+        src={noticia.image_url}
+        alt="Imagen de noticia no disponible"
+      />
+      <Card.Body>
+        <Card.Title>{noticia.title}</Card.Title>
+        <Card.Text>{noticia.description}</Card.Text>
+      </Card.Body>
+      <CardFooter className="text-center py-3">
+        <Button variant="primary" href={noticia.link}>
+          Ver noticia completa
+        </Button>
+      </CardFooter>
+    </Card>
   );
 };
 
